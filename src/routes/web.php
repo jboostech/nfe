ae8c4597453dedbfc6de49a782fadc1ade48bf05
Joao Romeiro <j.romeiro@live.com>
2022-08-05 13:37:57 -0300

    Corrige bug de rota

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,8 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('nfe', function () {
-    echo "Olá NF-e";
-});
-
 Route::get('/boostech/nfe', [Boostech\Nfe\Controllers\HnfexController::class, 'index'])->name('boostech_nfe.index');
